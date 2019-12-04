5a094ada5d1c0a90866b35117f0ad048faba9b96
wangliu1102 <wangliu.ah@qq.com>
2019-12-04 17:19:16 +0800

    有选项卡，无iframe内存溢出，故修复之前iframe内存溢出的代码

--- src/main/webapp/static/js/contabs.js
+++ src/main/webapp/static/js/contabs.js
@@ -163,21 +163,21 @@ $(function () {
 
             // 添加选项卡对应的iframe
             var str1 = '<iframe class="J_iframe" name="iframe' + dataIndex + '" width="100%" height="100%" src="' + dataUrl + '" frameborder="0" data-id="' + dataUrl + '" seamless></iframe>';
-            // $('.J_mainContent').find('iframe.J_iframe').hide().parents('.J_mainContent').append(str1);
+            $('.J_mainContent').find('iframe.J_iframe').hide().parents('.J_mainContent').append(str1);
             /**
              * 去掉所有layer弹框，避免内存泄漏
              * @type {jQuery}
              */
-            var jframe = $('.J_mainContent').find('iframe.J_iframe');
-            if(jframe.length > 0 && jframe[0].contentWindow.layer){
-                jframe[0].contentWindow.layer.closeAll();
-                //设置为空 垃圾回收掉
-                jframe[0].contentWindow.layer = null;
-                jframe[0].contentWindow.layui = null;
-            }
-            // * 去掉所有iframe，避免内存泄漏
-            $('.J_mainContent').find('iframe.J_iframe').remove();
-            $('.J_mainContent').append(str1);
+            // var jframe = $('.J_mainContent').find('iframe.J_iframe');
+            // if(jframe.length > 0 && jframe[0].contentWindow.layer){
+            //     jframe[0].contentWindow.layer.closeAll();
+            //     //设置为空 垃圾回收掉
+            //     jframe[0].contentWindow.layer = null;
+            //     jframe[0].contentWindow.layui = null;
+            // }
+            // // * 去掉所有iframe，避免内存泄漏
+            // $('.J_mainContent').find('iframe.J_iframe').remove();
+            // $('.J_mainContent').append(str1);
 
             //显示loading提示
             var loading = layer.load();
